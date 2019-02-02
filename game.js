a7c115641706f05396c6a5ca3b633945df4272e4
Gary Purbrick <gary.purbrick@heathwallace.com>
2019-02-02 10:13:43 +0000

    Adding first instance of basic game

---
+++ game.js
@@ -0,0 +1,10 @@
+const generateNumber = () => {
+
+	const min = 1;
+	const max = 10;
+
+	return Math.ceil(Math.random() * (max - min)) + min;
+
+}
+
+console.log("Higher or lower than" + generatedNumber + "?";
